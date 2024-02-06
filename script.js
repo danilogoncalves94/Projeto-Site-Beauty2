74a3e6aaf345f5c618db19004fd8636483fcd979
danilogoncalves94 <82965321+danilogoncalves94@users.noreply.github.com>
2024-02-06 14:07:09 -0300

    up

--- script.js
+++ script.js
@@ -5,68 +5,136 @@ function f1() {
 }
 
 $(document).ready(function () {
-    ScrollReveal().reveal('.titulo', {
-        origin: 'left',
-        distance: '50px',
-        duration: 1000,
-        delay: 300,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    /*ScrollReveal().reveal('.subtitulo', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1000,
-        delay: 900,
-        easing: 'ease-in-out',
-        reset: false,
-    })*/
-    ScrollReveal().reveal('.cabelo1', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1000,
-        delay: 300,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    ScrollReveal().reveal('.cabelo2', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1000,
-        delay: 1000,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    ScrollReveal().reveal('.cabelo3', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1000,
-        delay: 1500,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    ScrollReveal().reveal('.cabelo4', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1000,
-        delay: 2000,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    ScrollReveal().reveal('.logowhats', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1500,
-        delay: 300,
-        easing: 'ease-in-out',
-        reset: false,
-    })
-    ScrollReveal().reveal('.logoinsta', {
-        origin: 'bottom',
-        distance: '50px',
-        duration: 1100,
-        delay: 600,
-        easing: 'ease-in-out',
-        reset: false,
-    })
+    if (window.innerWidth > 479) {
+        ScrollReveal().reveal('.titulo', {
+            origin: 'left',
+            distance: '50px',
+            duration: 1000,
+            delay: 300,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        /*ScrollReveal().reveal('.subtitulo', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 900,
+            easing: 'ease-in-out',
+            reset: false,
+        })*/
+        ScrollReveal().reveal('.cabelo1', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 300,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo2', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 1000,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo3', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 1700,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo4', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 2400,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.logowhats', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1500,
+            delay: 300,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.logoinsta', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1100,
+            delay: 600,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+    } else {
+        ScrollReveal().reveal('.titulo', {
+            origin: 'left',
+            distance: '50px',
+            duration: 1000,
+            delay: 300,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        /*ScrollReveal().reveal('.subtitulo', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 900,
+            easing: 'ease-in-out',
+            reset: false,
+        })*/
+        ScrollReveal().reveal('.cabelo1', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 500,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo2', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 500,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo3', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 500,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.cabelo4', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1000,
+            delay: 500,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.logowhats', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1500,
+            delay: 300,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+        ScrollReveal().reveal('.logoinsta', {
+            origin: 'bottom',
+            distance: '50px',
+            duration: 1100,
+            delay: 600,
+            easing: 'ease-in-out',
+            reset: false,
+        })
+    }
+    
 })
